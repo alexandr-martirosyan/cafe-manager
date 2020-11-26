252f702d0f99c73ecb5d940580ca50e590a3dae7
Alexandr <alexandr.martirosyan2000@gmail.com>
2020-11-26 06:48:59 +0400

    m

--- src/test/java/com/company/cafemanager/CafeManagerDaoImplApplicationTests.java
+++ src/test/java/com/company/cafemanager/CafeManagerDaoImplApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CafeManagerApplicationTests {
+class CafeManagerDaoImplApplicationTests {
 
     @Test
     void contextLoads() {
